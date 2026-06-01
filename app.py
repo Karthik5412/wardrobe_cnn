203e5702865a07dfdefcc81681a7fdca1dde2b37
Karthik Kurucharlapati <155161592+Karthik5412@users.noreply.github.com>
2026-06-01 21:21:16 +0530

    just started cv portion

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 import numpy as np
+import cv2 as cv
 
 
 model = load_model("models\cnn.keras")
 
 img= r"data\clothing-dataset-small\validation\shoes\5e577f40-dd22-4b40-9827-ce6cae5ac3fd.jpg"
 
-img = tf.keras.utils.load_img(img, target_size=(224,224))
-img = tf.keras.utils.img_to_array(img)
-img = tf.expand_dims(img,0)
-prediction = model.predict(img)
+# img = tf.keras.utils.load_img(img, target_size=(224,224))
+# img = tf.keras.utils.img_to_array(img)
+# img = tf.expand_dims(img,0)
+# prediction = model.predict(img)
 
-result = tf.nn.softmax(prediction)
+# result = tf.nn.softmax(prediction)
 
-print(result)
-class_names = ['dress','hat','longsleeve','outwear','pants','shirt','shoes','shorts','skirt','t-shirt']
+# print(result)
+# class_names = ['dress','hat','longsleeve','outwear','pants','shirt','shoes','shorts','skirt','t-shirt']
 
-print(class_names[np.argmax(result)])
+# print(class_names[np.argmax(result)])
 
+cv.readim
